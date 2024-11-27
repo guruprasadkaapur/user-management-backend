4ff39bd9405e52113da95d1ee907fd7e76318c23
guruprasadkaapur <kapruguuru123@gmail.com>
2024-11-27 19:24:39 +0530

    latest

---
+++ controllers/userController.js
@@ -0,0 +1,47 @@
+const User = require('../models/User');
+
+// Get all users
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// Add a user
+exports.addUser = async (req, res) => {
+  const { name, email, phone } = req.body;
+  try {
+    const newUser = new User({ name, email, phone });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// Update a user
+exports.updateUser = async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.json(updatedUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// Delete a user
+exports.deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.json({ message: 'User deleted' });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
